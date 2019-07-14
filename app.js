56f3a06e16180edf5ab9a3ff9bd0c05bda2f446a
ankitmhn <ankit.m@hurestic.com>
2019-07-14 14:44:16 +0530

    Updated with grapiql enabled

--- app.js
+++ app.js
@@ -1,19 +1,42 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const graphQlHttp = require("express-graphql");
-const {buildSchema} = require("graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
 app.use(bodyParser.json());
 
-app.use("/", (req, res, next) => {
-    res.send("Hello World!");
-});
+app.use(
+  "/graphql",
+  graphQlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        type RootMutation {
+            createEvent(name: String): String
+        }
+        schema{
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return ["cooking", "sports", "music"];
+      },
+      createEvent: args => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
 
-app.use("/graphql", graphQlHttp({
-    schema: null,
-    rootValue: {}
-}));
+app.use("/", (req, res, next) => {
+  res.send("Hello World!");
+});
 
 app.listen(3000);
